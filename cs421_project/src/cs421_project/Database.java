36baa42198d118d8cdc4751a497b3ab6db97a2c8
Dylan <dcdrake@email.wm.edu>
2014-04-22 02:24:55 -0400

    Lists populated, ready to create queries.

--- cs421_project/src/cs421_project/Database.java
+++ cs421_project/src/cs421_project/Database.java
@@ -1,5 +1,12 @@
 package cs421_project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -9,12 +16,14 @@ import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Table;
 
 public class Database {
 
 	protected Shell Database;
 	private String[] states;
 	private String[] ratings;
+	private String[] schools;
 
 	/**
 	 * Launch the application.
@@ -31,8 +40,9 @@ public class Database {
 
 	/**
 	 * Open the window.
+	 * @throws SQLException 
 	 */
-	public void open() {
+	public void open() throws SQLException {
 		Display display = Display.getDefault();
 		createContents();
 		Database.open();
@@ -46,56 +56,107 @@ public class Database {
 
 	/**
 	 * Create contents of the window.
+	 * @throws SQLException 
 	 */
-	protected void createContents() {
+	protected void createContents() throws SQLException {
 		Database = new Shell();
-		Database.setSize(432, 279);
+		Database.setSize(486, 357);
 		Database.setText("Database GUI");
-		
+
+		String url = "jdbc:postgresql://localhost:63333/jagabel_project";
+		Properties props = new Properties();
+		props.setProperty("user","jagabel");
+		props.setProperty("password","tnO81cW");
+		Connection conn = DriverManager.getConnection(url, props);
+		if(conn.isValid(0)){
+			System.out.println("Connection Established");
+		}
+
 		states = new String[] {"AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI",
 				"ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO",
 				"MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI",
 				"SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"};
-		
+
 		ratings = new String[] {"0", "1", "2", "3", "4"};
-		
+
+		schools = new String[] {"Brown University", "California Institute of Technology", 
+				"California Polytechnic State University", "Carnegie Mellon University", 
+				"Columbia University", "Cornell University", "Georgia Institute of Technology", 
+				"Harvard University", "Harvey Mudd College", "Massachusetts Institute of Technology", 
+				"Princeton University", "Purdue University", "Rensselaer Polytechnic Institute", 
+				"Rice University", "Stanford University", "University of California - Los Angeles", 
+				"University of California - San Diego", "University of California at Berkeley", 
+				"University of Illinois - Urbana-Champaign", "University of Maryland - College Park", 
+				"University of Massachusetts - Amherst", "University of Michigan - Ann Arbor", 
+				"University of North Carolina - Chapel Hill", "University of Pennsylvania", 
+				"University of Southern California", "University of Texas - Austin", "University of Washington", 
+				"University of Waterloo", "University of Wisconsin - Madison", "Virginia Tech"};
+
 		Label lblUniversity = new Label(Database, SWT.NONE);
 		lblUniversity.setBounds(10, 10, 59, 14);
 		lblUniversity.setText("University");
-		
+
 		Label lblNewLabel = new Label(Database, SWT.NONE);
-		lblNewLabel.setBounds(99, 69, 59, 14);
+		lblNewLabel.setBounds(99, 157, 59, 19);
 		lblNewLabel.setText("City");
-		
-		List list = new List(Database, SWT.BORDER);
-		list.setBounds(10, 30, 98, 33);
-		
-		List state_list = new List(Database, SWT.BORDER);
-		state_list.setBounds(10, 92, 59, 60);
-		
+
+		List school_list = new List(Database, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		school_list.setBounds(10, 30, 280, 100);
+
+		for(int i=0; i<schools.length; i++)
+			school_list.add(schools[i]);
+
+		List state_list = new List(Database, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		state_list.setBounds(10, 177, 59, 92);
+
 		for(int i=0; i<states.length; i++)
-			  state_list.add(states[i]);
-		
-		List list_2 = new List(Database, SWT.BORDER);
-		list_2.setBounds(99, 92, 98, 60);
-		
+			state_list.add(states[i]);
+
+		List city_list = new List(Database, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		city_list.setBounds(99, 177, 191, 92);
+
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("SELECT DISTINCT city FROM business ORDER BY city");
+		while (rs.next())
+		{
+			String city  = rs.getString("city");
+			city_list.add(city);
+		} rs.close();
+		st.close();
+
 		Label lblState = new Label(Database, SWT.NONE);
-		lblState.setBounds(10, 69, 59, 14);
+		lblState.setBounds(10, 157, 59, 14);
 		lblState.setText("State");
-		
-		List rating_list = new List(Database, SWT.BORDER);
-		rating_list.setBounds(300, 30, 98, 53);
-		
+
+		List rating_list = new List(Database, SWT.BORDER | SWT.V_SCROLL);
+		rating_list.setBounds(345, 30, 98, 53);
+
 		for(int i=0; i<ratings.length; i++)
-			  rating_list.add(ratings[i]);
-		
+			rating_list.add(ratings[i]);
+
 		Label lblRatingAbove = new Label(Database, SWT.NONE);
-		lblRatingAbove.setBounds(300, 10, 77, 19);
+		lblRatingAbove.setBounds(345, 10, 87, 19);
 		lblRatingAbove.setText("Rating above:");
-		
+
 		Button btnNewButton = new Button(Database, SWT.NONE);
-		btnNewButton.setBounds(172, 219, 94, 28);
+		btnNewButton.setBounds(195, 297, 94, 28);
 		btnNewButton.setText("Search");
+
+		List category_list = new List(Database, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		category_list.setBounds(320, 177, 156, 92);
+
+		Statement st2 = conn.createStatement();
+		ResultSet rs2 = st2.executeQuery("SELECT DISTINCT category FROM business_categories ORDER BY category");
+		while (rs2.next())
+		{
+			String category  = rs2.getString("category");
+			category_list.add(category);
+		} rs2.close();
+		st2.close();
+
+		Label lblCategories = new Label(Database, SWT.NONE);
+		lblCategories.setBounds(320, 157, 69, 19);
+		lblCategories.setText("Categories");
 
 	}
 }
--- cs421_project/src/cs421_project/HelloWorld.java
+++ cs421_project/src/cs421_project/HelloWorld.java
@@ -10,47 +10,32 @@ import java.util.Properties;
 
 import javax.swing.JFrame;
 
-public class HelloWorld extends JFrame{
+public class HelloWorld{
 	
-	public HelloWorld() {
-        
-	       setTitle("cs421");
-	       setSize(300, 200);
-	       setLocationRelativeTo(null);
-	       setDefaultCloseOperation(EXIT_ON_CLOSE);        
-	    }
 
 	public static void main(String[] args) throws SQLException {
 		
-		JFrame f = new HelloWorld();
-	    f.setVisible(true);
 	    
 		String url = "jdbc:postgresql://localhost:63333/jagabel_project";
 		Properties props = new Properties();
-		props.setProperty("user","USER");
-		props.setProperty("password","PASSWORD");
+		props.setProperty("user","jagabel");
+		props.setProperty("password","tnO81cW");
 		//props.setProperty("ssl","true");
 		Connection conn = DriverManager.getConnection(url, props);
 		if(conn.isValid(0)){
 			System.out.println("Connection Established");
 		}
 		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("SELECT * FROM school");
+		ResultSet rs = st.executeQuery("SELECT DISTINCT category FROM business_categories ORDER BY category");
 		while (rs.next())
 		{
-			System.out.print("Column 1 returned ");
-			System.out.println(rs.getString(1));
-			int school_id  = rs.getInt("school_id");
-		    int latitude = rs.getInt("latitude");
-		    String state = rs.getString("state");
-		    System.out.println("school_id: " + school_id);
-		    System.out.println("latitude: " + latitude);
-		    System.out.println("state: " + state);
+			String category  = rs.getString("category");
+		    System.out.print('"' + category + '"'+ ", ");
 		} rs.close();
 		st.close();
 
 		//Example insert statement
-		String query = "INSERT INTO school VALUES (?, ?, ?, ?, ?, ?, ?)";
+		/*String query = "INSERT INTO school VALUES (?, ?, ?, ?, ?, ?, ?)";
 		PreparedStatement p = null;
 		p = conn.prepareStatement(query);
 		p.setInt(1, 19);
@@ -105,7 +90,7 @@ public class HelloWorld extends JFrame{
 		} rs.close();
 		st.close();
 
-		conn.close(); 	
+		conn.close(); 	*/
 	}
 
 }
