2b50c82fecb7a2cabe92476c98f2c2b9098094d7
Dylan <dcdrake@email.wm.edu>
2014-04-24 03:41:47 -0400

    got reviews to work.

--- cs421_project/src/cs421_project/Database.java
+++ cs421_project/src/cs421_project/Database.java
@@ -32,16 +32,17 @@ public class Database {
 	private String[] states;
 	private String[] ratings;
 	private String[] schools;
-	
+
 	private List city_list;
 	private Text text;
-	
+
 	private String schoolResult = "";
 	private String cityResult = "";
 	private String stateResult = "";
 	private String cateResult = "";
 	private String ratingResult = "";
-	
+	private Text review_text;
+
 	/**
 	 * Launch the application.
 	 * @param args
@@ -75,7 +76,7 @@ public class Database {
 	 */
 	protected void createContents() throws SQLException {
 		Database = new Shell();
-		Database.setSize(600, 475);
+		Database.setSize(1300, 575);
 		Database.setText("Database GUI");
 
 		String url = "jdbc:postgresql://localhost:63333/jagabel_project";
@@ -112,32 +113,32 @@ public class Database {
 		lblUniversity.setText("University");
 
 		Label lblNewLabel = new Label(Database, SWT.NONE);
-		lblNewLabel.setBounds(399, 28, 59, 19);
+		lblNewLabel.setBounds(477, 28, 59, 19);
 		lblNewLabel.setText("City");
 
 		final List school_list = new List(Database, SWT.BORDER | SWT.V_SCROLL);
-		school_list.setBounds(10, 48, 280, 100);
+		school_list.setBounds(10, 48, 314, 100);
 
 		for(int i=0; i<schools.length; i++)
 			school_list.add(schools[i]);
 
 		final List state_list = new List(Database, SWT.BORDER | SWT.V_SCROLL);
-		state_list.setBounds(334, 48, 59, 100);
+		state_list.setBounds(384, 48, 59, 100);
 
 		for(int i=0; i<states.length; i++)
 			state_list.add(states[i]);
 
 		city_list = new List(Database, SWT.BORDER | SWT.V_SCROLL);
-		city_list.setBounds(399, 48, 191, 100);
+		city_list.setBounds(477, 48, 191, 100);
 
 		state_list.addSelectionListener(new SelectionListener() {
 			public void widgetSelected(SelectionEvent event) {
-				
+
 				try {
 
 					String[] state_array = state_list.getSelection();
 					city_list.removeAll();
-					
+
 					Statement st;
 					st = conn.createStatement();
 					String state = "'" +state_array[0] + "'";
@@ -153,7 +154,7 @@ public class Database {
 					e.printStackTrace();
 				}catch (ArrayIndexOutOfBoundsException AE)
 				{
-					
+
 				}
 
 			}
@@ -166,7 +167,7 @@ public class Database {
 		});
 
 		Label lblState = new Label(Database, SWT.NONE);
-		lblState.setBounds(334, 28, 59, 14);
+		lblState.setBounds(384, 28, 59, 14);
 		lblState.setText("State");
 
 		final List rating_list = new List(Database, SWT.BORDER | SWT.V_SCROLL);
@@ -180,7 +181,7 @@ public class Database {
 		lblRatingAbove.setText("Rating above:");
 
 		Button searchButton = new Button(Database, SWT.NONE);
-		searchButton.setBounds(156, 425, 94, 28);
+		searchButton.setBounds(144, 515, 94, 28);
 		searchButton.setText("Search");
 
 		final List category_list = new List(Database, SWT.BORDER | SWT.V_SCROLL);
@@ -200,18 +201,18 @@ public class Database {
 		lblCategories.setText("Categories");
 
 		Button btnNewButton_1 = new Button(Database, SWT.NONE);
-		btnNewButton_1.setBounds(256, 425, 133, 28);
+		btnNewButton_1.setBounds(244, 515, 133, 28);
 		btnNewButton_1.setText("I'm Feeling Lucky");
 
 		text = new Text(Database, SWT.BORDER | SWT.V_SCROLL);
-		text.setBounds(310, 317, 280, 102);
+		text.setBounds(310, 317, 280, 159);
 
 		Label lblResults = new Label(Database, SWT.NONE);
 		lblResults.setBounds(10, 296, 59, 14);
 		lblResults.setText("Results:");
 
-		final List resultsList = new List(Database, SWT.BORDER);
-		resultsList.setBounds(10, 316, 244, 103);
+		final List resultsList = new List(Database, SWT.BORDER | SWT.V_SCROLL);
+		resultsList.setBounds(10, 316, 244, 160);
 
 		Label lblDetailedInformation = new Label(Database, SWT.NONE);
 		lblDetailedInformation.setBounds(310, 296, 133, 14);
@@ -222,7 +223,7 @@ public class Database {
 		lblSelect.setText("Select:");
 
 		Label lblOr = new Label(Database, SWT.NONE);
-		lblOr.setBounds(299, 80, 59, 14);
+		lblOr.setBounds(345, 80, 59, 14);
 		lblOr.setText("OR");
 
 		Label lblWith = new Label(Database, SWT.NONE);
@@ -233,13 +234,45 @@ public class Database {
 		lblAnd.setBounds(219, 211, 59, 14);
 		lblAnd.setText("AND");
 
-		
+		Button clearButton = new Button(Database, SWT.NONE);
+		clearButton.setBounds(383, 515, 114, 28);
+		clearButton.setText("Clear Selection");
+
+		review_text = new Text(Database, SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		review_text.setBounds(662, 249, 610, 227);
+
+		Label lblReviews = new Label(Database, SWT.NONE);
+		lblReviews.setBounds(662, 229, 59, 14);
+		lblReviews.setText("Reviews:");
+
+		clearButton.addSelectionListener(new SelectionListener(){
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+			}
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				city_list.deselectAll();
+				state_list.deselectAll();
+				school_list.deselectAll();
+				category_list.deselectAll();
+				rating_list.deselectAll();
+
+				schoolResult = "";
+				review_text.setText("");
+
+			}
+
+		});
+
+
 		// Listener section
-		
+
 		searchButton.addSelectionListener(new SelectionListener() {
 
 			public void widgetSelected(SelectionEvent event) {
-				
+
 				Statement s;
 				try {
 					s = conn.createStatement();
@@ -249,26 +282,26 @@ public class Database {
 					{
 						resultsList.add(r.getString(1));
 					}	
-					
+
 					r.close(); 
 					s.close();
-					
+
 					if (resultsList.getItems().length == 0)
 					{
 						resultsList.add("No results found");
 					}
-					
+
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} 
-				
+
 			}
 
 			public void widgetDefaultSelected(SelectionEvent event) {
 			}
 		});
-		
+
 		school_list.addSelectionListener(new SelectionListener(){
 
 			@Override
@@ -280,28 +313,28 @@ public class Database {
 				}
 				catch (ArrayIndexOutOfBoundsException AE)
 				{
-					
+
 				}
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
-		
+
 		city_list.addSelectionListener(new SelectionListener(){
 
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 				try
 				{
 					cityResult = city_list.getSelection()[0];
-					
+
 					if (cityResult.contains("'"))
 					{
 						cityResult = cityResult.substring(0, cityResult.indexOf("'")) +
@@ -312,20 +345,20 @@ public class Database {
 				}
 				catch (ArrayIndexOutOfBoundsException AE)
 				{
-					
+
 				}
-				
+
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
-				
-			});
-		
+
+		});
+
 		state_list.addSelectionListener(new SelectionListener(){
 
 			@Override
@@ -337,51 +370,51 @@ public class Database {
 				}
 				catch (Exception AE)
 				{
-					
+
 				}
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 
 		});
-		
+
 		category_list.addSelectionListener(new SelectionListener(){
 
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
 				try{
-					
+
 					cateResult = category_list.getSelection()[0];
 					if (cateResult.contains("'"))
 					{
 						cateResult = cateResult.substring(0, cateResult.indexOf("'"))
 								+ "''" + cateResult.substring(cateResult.indexOf("'")
 										+ 1, cateResult.length());
-						
+
 					}
-					
+
 				}
 				catch (ArrayIndexOutOfBoundsException AE)
 				{
-					
+
 				}
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
-			
+
 		});
-		
+
 		rating_list.addSelectionListener(new SelectionListener(){
 
 			@Override
@@ -396,18 +429,18 @@ public class Database {
 				}
 				catch (ArrayIndexOutOfBoundsException AE)
 				{
-					
+
 				}
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 		});
-		
+
 		resultsList.addSelectionListener(new SelectionListener(){
 
 			@Override
@@ -415,43 +448,43 @@ public class Database {
 				// TODO Auto-generated method stub
 				try
 				{	text.setText("");
-					fireSingleBusinessQuery(resultsList.getSelection()[0], conn);
+				fireSingleBusinessQuery(resultsList.getSelection()[0], conn);
 				}
 				catch (ArrayIndexOutOfBoundsException AE)
 				{
-			
+
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
 			}
-			
+
 		});
-		
+
 		btnNewButton_1.addSelectionListener(new SelectionListener(){
 
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub				
-								
+
 			}
-			
+
 		});
-		
+
 	}
-	
+
 	private String buildQuery()
 	{
 		String result = "";
@@ -460,84 +493,85 @@ public class Database {
 		String statePortion = "";
 		String schoolPortion = "";
 		String ratingPortion = "";
-		
+
 		boolean hasWhere = false;
-		
+
 		if (cateResult !="")
 		{
-				
-				catePortion = "business_categories.category = '" + 
-				cateResult + "' AND business_categories.business_id = business.business_id AND ";
-				
+
+			catePortion = "business_categories.category = '" + 
+					cateResult + "' AND business_categories.business_id = business.business_id AND ";
+
 			hasWhere = true;
 		}
-		
+
 		if (schoolResult != "")
 		{			schoolPortion = "school.name = '" + schoolResult + 
-					"' AND school.city = business.city AND ";
-			hasWhere = true;
+		"' AND school.city = business.city AND ";
+		hasWhere = true;
 		}
-		
+
 		if (cityResult != "")
 		{
 			cityPortion = "business.city = '" + cityResult + "' AND ";
 			hasWhere = true;
 		}
-		
+
 		if (stateResult != "" && cityPortion == "")
 		{
 			statePortion = "business.state = '" + stateResult + "' AND ";
 			hasWhere = true;
 		}
-		
+
 		if (ratingResult != "")
 		{
 			ratingPortion = "business.stars > " + ratingResult + " AND ";
 			hasWhere = true;
 		}
-		
+
 		if (hasWhere && (schoolPortion == "" && cityPortion == "" && statePortion == ""))
 		{
 			result = "SELECT DISTINCT business.name, business.stars FROM business, business_categories WHERE ";
 			result += catePortion;
-			result += "ORDER BY business.stars LIMIT 10 ";
+			result += "ORDER BY business.stars DESC LIMIT 10 ";
 		}
 		else if (hasWhere && schoolPortion == "")
 		{
 			result = "SELECT DISTINCT business.name, business.stars FROM business, business_categories WHERE "
-		+ cityPortion + statePortion + ratingPortion + catePortion;
+					+ cityPortion + statePortion + ratingPortion + catePortion;
 			result = result.substring(0, result.length() - 4);
-			result += "ORDER BY business.stars LIMIT 10 ";
+			result += "ORDER BY business.stars DESC LIMIT 10 ";
 		}
 		else if (hasWhere)
 		{
 			result = "SELECT DISTINCT business.name, business.stars FROM business, school,"
 					+ " business_categories WHERE " + schoolPortion + ratingPortion + catePortion;
-				result = result.substring(0, result.length() - 4) ;
-			
-			result += "ORDER BY business.stars LIMIT 10 ";
-			
+			result = result.substring(0, result.length() - 4) ;
+
+			result += "ORDER BY business.stars DESC LIMIT 10 ";
+
 		}
 		else
 		{
-			result = "SELECT DISTINCT business.name FROM business, business_categories ORDER BY business.stars LIMIT 10";
+			result = "SELECT DISTINCT business.name FROM business, business_categories ORDER BY business.stars DESC LIMIT 10";
 		}
-		
+
 		System.out.println(result);
 		return result;
 	}
-	
+
 	private void fireSingleBusinessQuery(String name, Connection conn) throws SQLException
 	{
 		Statement s = conn.createStatement();
+		//Statement s2 = conn.createStatement();
 		//For issues where business names contain the ' character
 		//postgres throws a fit and so they must be represented by ''
 		if (name.contains("'"))
 		{
 			name = name.substring(0, name.indexOf("'")) + "''" + 
-		name.substring(name.indexOf("'") + 1, name.length());
+					name.substring(name.indexOf("'") + 1, name.length());
 		}
-		
+
 		//Very, very messy reconstruction of the query from buildQuery().
 		//In essence, takes the query from buildQuery and then modifies it so that
 		//Only the desired business's data is returned (to avoid for example giving info
@@ -547,14 +581,25 @@ public class Database {
 		if (query.substring(query.indexOf("//") - 1, query.indexOf("//")).equals("."));
 		{
 			query = query.replaceFirst("business.", "");
-			
+
 			query = query.replaceFirst("//", "business.name, business.address, "
 					+ "business.city, business.state, business.stars, "
 					+ "business.review_count, business.open");
 		}
 		query = query.substring(0, query.indexOf("ORDER"))+ " AND business.name = '" + name + "'";
-		
-		ResultSet r = s.executeQuery(query);
+
+
+		/*Business Reviews query */
+		ResultSet r = s.executeQuery("SELECT review.text, review.votes FROM review, business WHERE business.business_id = review.business_id "
+				+ "AND review.business_id = ("
+				+ "SELECT business_id FROM business WHERE business.name = '" + name + "'" +") "
+				+ "ORDER BY review.votes DESC LIMIT 3");
+		r.next();
+		String raw_review_string = r.getString("text");
+		review_text.setText(raw_review_string);
+
+		/*Additional info query*/
+		r = s.executeQuery(query);
 		String resultsString = "";
 		r.next();
 		resultsString = "Name: " + r.getString(1) + "\n";
@@ -567,12 +612,13 @@ public class Database {
 			resultsString += "Currently Open";
 		else
 			resultsString += "Currently Closed";
-		
+
 		text.setText(resultsString);
-		
+
+
 		r.close();
 		s.close();
-		
+
 	}
-	
+
 }
